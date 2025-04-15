bb9279aa892feed37b4cbd6a963a9e562f5acb49
Haim <haim.pizarro@gmail.com>
2025-04-15 19:39:36 -0400

    comenzando modificaciones 2da evaluacion

--- src/main/java/com/example/viaje/mascota/viajemascota/models/ViajeMascota.java
+++ src/main/java/com/example/viaje/mascota/viajemascota/models/ViajeMascota.java
@@ -1,13 +1,29 @@
 package com.example.viaje.mascota.viajemascota.models;
 
 
+jakarta.persistence.entity;
+jakarta.persistence.id;
+jakarta.persistence.table;
+
 import lombok.Data;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+jakarta.validation.constrains.NotBlank;
+jakarta.validation.constrains.NotNull;
+jakarta.validation.constrains.Size;
+jakarta.validation.constrains.Min;
+jakarta.validation.constrains.Max;
+jakarta.validation.constrains.Positive;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@EntityScan
+@Table(name = "viajemascota") //Falta agregar base de datos
 public class ViajeMascota {
     private Long id;
     private String nombremascota;
