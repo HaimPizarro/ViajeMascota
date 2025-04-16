9b6ce37333522ca83ae56518444964b6edcd3f4a
Haim <haim.pizarro@gmail.com>
2025-04-15 20:26:12 -0400

    Exceptions listas, comenzando con services

---
+++ src/main/java/com/example/viaje/mascota/viajemascota/exceptions/ViajesNotFound.java
@@ -0,0 +1,12 @@
+package com.example.viaje.mascota.viajemascota.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus; 
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ViajesNotFound extends RuntimeException {
+    public ViajesNotFound(Long id) {
+
+        super("No se ha encontrado el pelicula con id: " + id);
+    }
+}
