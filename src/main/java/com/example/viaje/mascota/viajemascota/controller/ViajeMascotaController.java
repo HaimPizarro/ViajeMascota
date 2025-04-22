bbbb2fe2b99aaddc19b687d04b2203b0e94e0718
HaimPizarro <haim.pizarro@gmail.com>
2025-04-21 21:29:00 -0400

    error solucionado requestmapping

--- src/main/java/com/example/viaje/mascota/viajemascota/controller/ViajeMascotaController.java
+++ src/main/java/com/example/viaje/mascota/viajemascota/controller/ViajeMascotaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/viajemascota")
+@RequestMapping("/viajemascota")
 public class ViajeMascotaController {
 
     private final ViajeMascotaServices viajeMascotaServices;
